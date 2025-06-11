ea750868c1d7390f873fe5a4684e62593cb89dbe
Maimoon <maimoon.ideal@gmail.com>
2025-06-11 06:58:41 +0500

    deleted files

--- bookai_project/book_ai/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'book_ai'
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('upload/', views.upload_document, name='upload_document'),
-]
